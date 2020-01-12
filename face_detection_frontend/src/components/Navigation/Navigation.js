7deccf2ed6921d2d5dec46aec65a5963d78e9306
naveen-ku <naveen2951996@gmail.com>
2020-01-13 03:01:23 +0530

    adding basic frontend part

---
+++ face_detection_frontend/src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigatoion = () => {
+    return (
+        <nav style={{display:'flex', justifyContent:'flex-end'}}>
+            <p>Sign Out</p>
+        </nav>
+    )
+}
+
+export default Navigatoion;
